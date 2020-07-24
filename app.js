f3e3d9f0d0ad58d498c199da048aeb30354a9196
Adam Alicki <adam@alicki.me>
2020-07-23 20:18:38 -0400

    Updating .gitigore.
    Added node.js engine version.

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
@@ -12,13 +10,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+app.listen(3000, function () {
+    console.log("Express server listening on port 3000");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
